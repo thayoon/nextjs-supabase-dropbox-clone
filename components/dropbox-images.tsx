ce9632ded15bb81f375eb27e75673a1acc1459f5
thayoon <thagyun@naver.com>
2025-03-12 23:07:16 +0900

    feat: supabase storage Delete

--- components/dropbox-images.tsx
+++ components/dropbox-images.tsx
@@ -1,9 +1,20 @@
 "use client";
 
-import { IconButton } from "@material-tailwind/react";
+import { IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteFile } from "actions/storageActions";
+import { queryClient } from "config/ReactQueryClientProvider";
 import { getImageUrl } from "utils/supabase/storage";
 
 export default function DropboxImage({ image }) {
+  const deleteFileMutation = useMutation({
+    mutationFn: deleteFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["images"],
+      });
+    },
+  });
   return (
     <div className="relative w-full flex flex-col gap-2 p-4 border border-gray-100 rounded-2xl shadow-md">
       {/* Image */}
@@ -20,8 +31,17 @@ export default function DropboxImage({ image }) {
 
       {/* trash Button */}
       <div className="absolute top-4 right-4">
-        <IconButton onClick={() => {}} color="red">
-          <i className="fas fa-trash" />
+        <IconButton
+          onClick={() => {
+            deleteFileMutation.mutate(image.name);
+          }}
+          color="red"
+        >
+          {deleteFileMutation.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-trash" />
+          )}
         </IconButton>
       </div>
     </div>
