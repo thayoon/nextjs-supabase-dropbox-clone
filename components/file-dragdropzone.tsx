c82b8a83fd9b6dd49b2e6032f5f7f215a8c1ac10
thayoon <thagyun@naver.com>
2025-03-12 23:21:59 +0900

    feat: drag and drop using react-dropzone

--- components/file-dragdropzone.tsx
+++ components/file-dragdropzone.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@material-tailwind/react";
+import { Button, Spinner } from "@material-tailwind/react";
 import { uploadFile } from "actions/storageActions";
 import { queryClient } from "config/ReactQueryClientProvider";
 import { useMutation } from "@tanstack/react-query";
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function FileDragDropZone() {
   const fileRef = useRef(null);
@@ -15,24 +16,30 @@ export default function FileDragDropZone() {
     },
   });
 
+  const onDrop = useCallback(async (acceptedFiles) => {
+    const file = acceptedFiles?.[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      const result = await uploadImageMutation.mutate(formData);
+    }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        const file = fileRef.current.files?.[0];
-        if (file) {
-          const formData = new FormData();
-          formData.append("file", file);
-          const result = await uploadImageMutation.mutate(formData);
-        }
-      }}
-      className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center"
+    <div
+      {...getRootProps()}
+      className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center cursor-pointer"
     >
-      <input ref={fileRef} type="file" className="" />
-      <p>파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요.</p>
-      <Button loading={uploadImageMutation.isPending} type="submit">
-        파일 업로드
-      </Button>
-    </form>
+      <input {...getInputProps()} />
+
+      {uploadImageMutation.isPending ? (
+        <Spinner />
+      ) : isDragActive ? (
+        <p>파일을 놓아주세요.</p>
+      ) : (
+        <p>파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요.</p>
+      )}
+    </div>
   );
 }
